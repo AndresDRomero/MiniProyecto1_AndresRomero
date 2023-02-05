f2279ce49ae44734bc67bfd00f8d9459f5f0fa5e
AndresDRomero <andresdromeroa99@gmail.com>
2023-02-05 01:17:32 -0400

    Create Carrousel

--- main.js
+++ main.js
@@ -4,3 +4,45 @@ function display_menu(){
         anchor[i].classList.toggle('desaparece');
     }
 }
+
+var imagenes = ['/Imagenes/img1.jpg', '/Imagenes/img2.jpg', '/Imagenes/img3.jpg', '/Imagenes/img4.jpg'],
+    cont = 0;
+
+function carrou(carrousel){
+    carrousel.addEventListener('click', e => {
+        let    atras = carrousel.querySelector('.atras')
+        let    adelante = carrousel.querySelector('.adelante')
+        let    img = carrousel.querySelector('img')
+        let    tgt = e.target;
+
+
+        if(tgt == atras){
+            if(cont > 0){
+                img.src = imagenes[cont-1];
+                cont --;
+
+            }else {
+                img.src = imagenes[imagenes.length -1];
+                cont = imagenes.length -1;
+            }
+
+        }else if(tgt == adelante){
+            if(cont < imagenes.length -1){
+                img.src = imagenes[cont+1];
+                cont ++;
+
+            }else {
+                img.src = imagenes[0];
+                cont = 0;
+            }
+
+        }
+    });
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let carrousel = document.querySelector('.carrousel');
+
+    carrou(carrousel);
+});
